f7796498ef5d05d266b9da40229c117c2faa75d7
Gina Häußge <gina@octoprint.org>
2021-01-22 11:45:39 +0100

    Dynamic line number width

--- src/components/LogView.js
+++ src/components/LogView.js
@@ -2,29 +2,28 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { FixedSizeList } from "react-window";
 
-const useStyles = makeStyles(theme => ({
-    log: {
-        flexGrow: 1,
-        overflow: "auto",
-        padding: 0,
-        "font-family": "'JetBrains Mono', 'Droid Sans Mono', monospace",
-    },
-    linenumber: {
-        display: "inline-block",
-        width: "5em",
-        "text-align": "right",
-        "margin-right": "1em",
-        "color": theme.palette.text.secondary
-    },
-    line: {
-
-    }
-}));
-
 export default function LogView(props) {
-    const classes = useStyles();
-
     const lines = props.content.split("\n");
+    const lineCount = lines.length;
+
+    const classes = makeStyles(theme => ({
+        log: {
+            flexGrow: 1,
+            overflow: "auto",
+            padding: 0,
+            "font-family": "'JetBrains Mono', 'Droid Sans Mono', monospace",
+        },
+        linenumber: {
+            display: "inline-block",
+            width: `${lineCount.toString().length}ch`,
+            "text-align": "right",
+            "margin-right": "1em",
+            "color": theme.palette.text.secondary
+        },
+        line: {
+    
+        }
+    }))();
 
     const Line = ({ index, style }) => (
         <span style={style}><span className={classes.linenumber}>{index+1}</span><span className={classes.line}>{lines[index]}</span></span>
@@ -35,7 +34,7 @@ export default function LogView(props) {
             <pre>
                 <FixedSizeList 
                     height={500}
-                    itemCount={lines.length}
+                    itemCount={lineCount}
                     itemSize={20}
                 >
                     {Line}
